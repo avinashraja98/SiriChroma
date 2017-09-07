619941eb848ab39dbdadd7e11302ee6f0bf2111b
Avinash Raja <avinashraja98@hotmail.com>
2017-09-07 15:13:28 -0230

    Initial Build

---
+++ LightingProject/MainWindow.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace LightingProject
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HueFeed = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Redlabel = new System.Windows.Forms.Label();
+            this.GreenLabel = new System.Windows.Forms.Label();
+            this.BlueLabel = new System.Windows.Forms.Label();
+            this.SaturationFeed = new System.Windows.Forms.Label();
+            this.MainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainMenuStrip
+            // 
+            this.MainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainMenuStrip.Name = "MainMenuStrip";
+            this.MainMenuStrip.Size = new System.Drawing.Size(284, 24);
+            this.MainMenuStrip.TabIndex = 0;
+            this.MainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.Exit);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // HueFeed
+            // 
+            this.HueFeed.AutoSize = true;
+            this.HueFeed.Location = new System.Drawing.Point(164, 35);
+            this.HueFeed.Name = "HueFeed";
+            this.HueFeed.Size = new System.Drawing.Size(54, 13);
+            this.HueFeed.TabIndex = 2;
+            this.HueFeed.Text = "Hue Feed";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // Redlabel
+            // 
+            this.Redlabel.AutoSize = true;
+            this.Redlabel.Location = new System.Drawing.Point(32, 35);
+            this.Redlabel.Name = "Redlabel";
+            this.Redlabel.Size = new System.Drawing.Size(30, 13);
+            this.Redlabel.TabIndex = 6;
+            this.Redlabel.Text = "Red:";
+            // 
+            // GreenLabel
+            // 
+            this.GreenLabel.AutoSize = true;
+            this.GreenLabel.Location = new System.Drawing.Point(32, 69);
+            this.GreenLabel.Name = "GreenLabel";
+            this.GreenLabel.Size = new System.Drawing.Size(39, 13);
+            this.GreenLabel.TabIndex = 7;
+            this.GreenLabel.Text = "Green:";
+            // 
+            // BlueLabel
+            // 
+            this.BlueLabel.AutoSize = true;
+            this.BlueLabel.Location = new System.Drawing.Point(32, 108);
+            this.BlueLabel.Name = "BlueLabel";
+            this.BlueLabel.Size = new System.Drawing.Size(31, 13);
+            this.BlueLabel.TabIndex = 8;
+            this.BlueLabel.Text = "Blue:";
+            // 
+            // SaturationFeed
+            // 
+            this.SaturationFeed.AutoSize = true;
+            this.SaturationFeed.Location = new System.Drawing.Point(164, 142);
+            this.SaturationFeed.Name = "SaturationFeed";
+            this.SaturationFeed.Size = new System.Drawing.Size(82, 13);
+            this.SaturationFeed.TabIndex = 9;
+            this.SaturationFeed.Text = "Saturation Feed";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.SaturationFeed);
+            this.Controls.Add(this.BlueLabel);
+            this.Controls.Add(this.GreenLabel);
+            this.Controls.Add(this.Redlabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.HueFeed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MainMenuStrip);
+            this.Controls.Add(this.label2);
+            this.MainMenuStrip = this.MainMenuStrip;
+            this.Name = "MainWindow";
+            this.Text = "SiriChroma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MainMenuStrip.ResumeLayout(false);
+            this.MainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label HueFeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Redlabel;
+        private System.Windows.Forms.Label GreenLabel;
+        private System.Windows.Forms.Label BlueLabel;
+        private System.Windows.Forms.Label SaturationFeed;
+    }
+}
+
---
+++ LightingProject/MainWindow.cs
@@ -0,0 +1,274 @@
+﻿using Corale.Colore.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using ColoreColor = Corale.Colore.Core.Color;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using Newtonsoft.Json.Linq;
+
+
+namespace LightingProject
+{
+
+    public partial class MainWindow : Form
+    {
+        MqttClient client;
+        double fH = 0,fS = 0, fV = 100, prev=0;
+            
+        public MainWindow() {
+            InitializeComponent();
+        }
+        public struct RGB
+        {
+            private byte _r;
+            private byte _g;
+            private byte _b;
+
+            public RGB(byte r, byte g, byte b)
+            {
+                this._r = r;
+                this._g = g;
+                this._b = b;
+            }
+
+            public byte R
+            {
+                get { return this._r; }
+                set { this._r = value; }
+            }
+
+            public byte G
+            {
+                get { return this._g; }
+                set { this._g = value; }
+            }
+
+            public byte B
+            {
+                get { return this._b; }
+                set { this._b = value; }
+            }
+
+            public bool Equals(RGB rgb)
+            {
+                return (this.R == rgb.R) && (this.G == rgb.G) && (this.B == rgb.B);
+            }
+        }
+
+        public struct HSV
+        {
+            private double _h;
+            private double _s;
+            private double _v;
+
+            public HSV(double h, double s, double v)
+            {
+                this._h = h;
+                this._s = s;
+                this._v = v;
+            }
+
+            public double H
+            {
+                get { return this._h; }
+                set { this._h = value; }
+            }
+
+            public double S
+            {
+                get { return this._s; }
+                set { this._s = value; }
+            }
+
+            public double V
+            {
+                get { return this._v; }
+                set { this._v = value; }
+            }
+
+            public bool Equals(HSV hsv)
+            {
+                return (this.H == hsv.H) && (this.S == hsv.S) && (this.V == hsv.V);
+            }
+        }
+
+        public static RGB HSVToRGB(HSV hsv)
+        {
+            double r = 0, g = 0, b = 0;
+
+            if (hsv.S == 0)
+            {
+                r = hsv.V;
+                g = hsv.V;
+                b = hsv.V;
+            }
+            else
+            {
+                int i;
+                double f, p, q, t;
+
+                if (hsv.H == 360)
+                    hsv.H = 0;
+                else
+                    hsv.H = hsv.H / 60;
+
+                i = (int)Math.Truncate(hsv.H);
+                f = hsv.H - i;
+
+                p = hsv.V * (1.0 - hsv.S);
+                q = hsv.V * (1.0 - (hsv.S * f));
+                t = hsv.V * (1.0 - (hsv.S * (1.0 - f)));
+
+                switch (i)
+                {
+                    case 0:
+                        r = hsv.V;
+                        g = t;
+                        b = p;
+                        break;
+
+                    case 1:
+                        r = q;
+                        g = hsv.V;
+                        b = p;
+                        break;
+
+                    case 2:
+                        r = p;
+                        g = hsv.V;
+                        b = t;
+                        break;
+
+                    case 3:
+                        r = p;
+                        g = q;
+                        b = hsv.V;
+                        break;
+
+                    case 4:
+                        r = t;
+                        g = p;
+                        b = hsv.V;
+                        break;
+
+                    default:
+                        r = hsv.V;
+                        g = p;
+                        b = q;
+                        break;
+                }
+
+            }
+
+            return new RGB((byte)(r * 255), (byte)(g * 255), (byte)(b * 255));
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                 client = new MqttClient("broker.mqttdashboard.com");
+                byte code = client.Connect(Guid.NewGuid().ToString());
+
+                ushort msgId = client.Subscribe(new string[] { "DonBridge/from/set/#" }, new byte[] { 2 });
+
+                client.MqttMsgSubscribed += client_MqttMsgSubscribed;
+                client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+                
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Failed to setup connection\n\n"+ex.Message);
+            }
+            Console.WriteLine("hi");
+        }
+
+
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string message = Encoding.UTF8.GetString(e.Message);
+            
+            try
+            {
+                dynamic msg = JObject.Parse(message);
+                UpdateDevices(msg);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Message received but failed to parse JSON\n\n" + ex.Message);
+                //Application.Exit();
+            }
+
+            
+
+
+        }
+
+        private void UpdateDevices(dynamic msg)
+        {
+            if (msg.characteristic == "Hue")
+            UpdateLabel(HueFeed, Convert.ToString(msg));
+
+            if (msg.characteristic == "Saturation")
+                UpdateLabel(SaturationFeed, Convert.ToString(msg));
+
+
+            Console.Write(msg.characteristic);
+            Console.WriteLine(msg.value);
+
+            if (msg.characteristic == "On" && msg.value == "False") fV = 0; else if (msg.characteristic == "On" && msg.value == "True") fV = prev;
+
+            if (msg.characteristic == "Hue") fH = msg.value;
+            if (msg.characteristic == "Saturation") fS = msg.value;
+            if (msg.characteristic == "Brightness") fV = msg.value;
+
+
+
+            HSV data = new HSV(fH, fS/100, fV/100);
+            RGB value = HSVToRGB(data);
+
+
+            UpdateLabel(label2, Convert.ToString(value.R));
+            UpdateLabel(label3, Convert.ToString(value.G));
+            UpdateLabel(label4, Convert.ToString(value.B));
+            
+      
+            var color = new ColoreColor((byte)value.R, (byte)value.G, (byte)value.B);
+            Chroma.Instance.SetAll(color);
+
+
+            if(fV!=0)prev = fV;
+
+
+        }
+
+        void UpdateLabel(Label lbl, String text)
+        {
+            if (lbl.InvokeRequired)
+            { lbl.Invoke(new Action<Label, String>(UpdateLabel), new object[] { lbl, text }); }
+            else
+            { lbl.Text = text; }
+        }
+
+        private void client_MqttMsgPublished(object sender, MqttMsgPublishedEventArgs e)
+        {
+            Console.WriteLine("MessageId = " + e.MessageId + " Published = " + e.IsPublished);
+        }
+        void client_MqttMsgSubscribed(object sender, MqttMsgSubscribedEventArgs e)
+        {
+            MessageBox.Show("Sucessfully Connected and Subscribed");
+        }
+    }
+}
